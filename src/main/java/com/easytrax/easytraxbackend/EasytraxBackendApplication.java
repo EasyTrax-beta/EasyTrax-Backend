a4476d40117e960b25f041d41ba56e774b83b2df
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-09-06 12:56:05 +0900

    init

---
+++ src/main/java/com/easytrax/easytraxbackend/EasytraxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easytrax.easytraxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytraxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasytraxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easytrax/easytraxbackend/EasytraxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easytrax.easytraxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasytraxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
