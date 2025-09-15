f0b50dcb03bc28f7f47e94dd1c7504a2d3af51bc
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-09-15 23:08:54 +0900

    feat/#12: JPA Auditing 적용

--- src/main/java/com/easytrax/easytraxbackend/EasytraxBackendApplication.java
+++ src/main/java/com/easytrax/easytraxbackend/EasytraxBackendApplication.java
@@ -2,8 +2,10 @@ package com.easytrax.easytraxbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EasytraxBackendApplication {
 
     public static void main(String[] args) {
